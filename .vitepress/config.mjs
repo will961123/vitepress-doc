9b13467445ac464f95a065f584662ce52af090a5
wupf <wupf@bjhzwq.com>
2024-09-04 15:25:09 +0800

    feat: init

---
+++ .vitepress/config.mjs
@@ -0,0 +1,70 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'wpf 的在线文档',
+  description: '在线文档',
+  head: [['link', { rel: 'icon', href: '/images/index/logo.svg' }]],
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: '/images/index/logo.svg',
+
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档',
+          },
+          modal: {
+            noResultsText: '未找到结果',
+            resetButtonTitle: '清除查询条件',
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+              closeText: '关闭',
+            },
+          },
+        },
+      },
+    },
+
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '文档示例', link: '/markdown-examples' },
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+    ],
+
+    sidebar: [
+      {
+        text: '文档示例',
+        items: [
+          { text: 'markdown 示例', link: '/markdown-examples' },
+          { text: 'api 示例', link: '/api-examples' },
+        ],
+      },
+      {
+        text: '文档示例2',
+        items: [
+          { text: 'markdown 示例', link: '/markdown-examples' },
+          { text: 'api 示例', link: '/api-examples' },
+        ],
+      },
+    ],
+
+    outline: {
+      label: '文章目录',
+      level: 'deep',
+    },
+
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright© 2024 wpf',
+    },
+
+  },
+})
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: wpf 的在线文档
+  text: 好记性不如烂笔头
+  tagline: 于暴雨中行走，伞是倒划天空的船
+  image: 
+    src: /images/index/logo.svg
+    alt: 背景图
+  actions:
+    - theme: brand
+      text: markdown 示例
+      link: /markdown-examples
+    - theme: alt
+      text: api 示例
+      link: /api-examples
+
+features:
+  - title: 💡 小建议
+    details: 选择永远大于努力
+  - title: 🧗 努力
+    details: 如果做一件事就努力把它做好
+  - title: 🤔 思考
+    details: 学会思考，不要人云亦云
+---
+
